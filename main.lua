37c4d42d86bb25a62e08539cc660a906841634d7
Rin449 <giangmrk64@gmail.com>
2025-10-05 06:54:54 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
 if game.PlaceId == 127742093697776 then
-    loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/4717d34115333bc29d543439fa58be3e"))()
+    loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/b7733e30b330bbe6bee28887f2ec7898"))()
 else
     game.Players.LocalPlayer:Kick("This game is not supported.")
 end
